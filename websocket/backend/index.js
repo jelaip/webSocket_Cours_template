e78e98a7860405445eb702b8f14da8597d421e0c
jelaip <37291886+jelaip@users.noreply.github.com>
2024-06-05 12:12:15 +0200

    update leave join

--- websocket/backend/index.js
+++ websocket/backend/index.js
@@ -11,6 +11,9 @@ const io = new Server(server, {
         origin: '*',
         }
 })
+
+let SaveDataRoom = [];  
+
 app.use(cors())
 app.get('/', (req, res) => {
     res.json('ip address: http://' + ip.address()+':'+PORT);    
@@ -28,20 +31,34 @@ io.on('connection', (socket) => {
         io.emit('message', msg);
     });
     
-    socket.on('room', (room, msg) => {
-        console.log('room: ' + room + ' message: ' + msg);
-        io.to(room).emit('message', msg);
+    socket.on('room', (room, user, msg) => {
+        console.log('room: ' + room + ' message: ' + msg + ' user: ' + user);
+        if (SaveDataRoom[room] === undefined) {
+            SaveDataRoom[room] = [];
+        }
+        SaveDataRoom[room].push(user + ' : ' + msg);
+        io.to(room).emit('message', msg,user);
     });
 
-    socket.on('join', (room) => {
-        console.log('join room: ' + room);
+    socket.on('join', (room, nameUser) => {
+        console.log('join room: ' + room + ' name: ' + nameUser);
         socket.join(room);
-        io.to(room).emit('join', room);
+        if (SaveDataRoom[room] === undefined) {
+            SaveDataRoom[room] = [];
+        }
+        SaveDataRoom[room].push(nameUser + ' join ' + room);
+        socket.emit('getHistory', SaveDataRoom[room]);
+        //io.to(room).emit('join', room, nameUser);
+        socket.to(room).emit('joinUser', room, nameUser);
     });
-    socket.on('leave', (room) => {
-        console.log('leave room: ' + room);
+    socket.on('leave', (room, user) => {
+        console.log('leave room: ' + room+  ' user: ' + user);
+        if (SaveDataRoom[room] === undefined) {
+            SaveDataRoom[room] = [];
+        }
+        SaveDataRoom[room].push(user + ' leave ' + room);
         socket.leave(room);
-        io.to(room).emit('leave', room);
+        io.to(room).emit('leaveUser', room, user);
     });
 })
 
--- websocket/front/script.js
+++ websocket/front/script.js
@@ -1,26 +1,57 @@
+let nameUser = '';
 let room = '';
 let socketid = '';
 const roomArea = document.querySelector('#room');
 const messageArea = document.querySelector('#message');
+const nameArea = document.querySelector('#name');
 const socket = io('http://localhost:3000');
 socket.on('connect', () => {
     console.log('Connected');
 });
-socket.on('message', (data) => {
+socket.on('message', (data, user) => {
     console.log(data);
-    document.querySelector('.data').innerText = data;
+    const div = document.createElement('div');
+    div.innerHTML = `<p>${user} : ${data}</p>`;
+    document.querySelector('#data').appendChild(div);
 });
 socket.on('disconnect', () => {
     console.log('Disconnected');
 });
 
+socket.on('getHistory', (data) => {
+    document.querySelector('#data').innerHTML = '';
+    const div = document.createElement('div');
+    data.forEach((msg) => {
+        div.innerHTML += `<p>${msg}</p>`;
+    });
+    document.querySelector('#data').appendChild(div);
+});
+
+socket.on('joinUser', (room, user) => {
+    console.log('join room: ' + room + ' user: ' + user);
+    const div = document.createElement('div');
+    div.innerHTML = `<p>${user} join ${room}</p>`;
+    document.querySelector('#data').appendChild(div);
+})
+
+socket.on('leaveUser', (room, user) => {
+    console.log('leave room: ' + room + ' user: ' + user);
+    const div = document.createElement('div');
+    div.innerHTML = `<p>${user} leave ${room}</p>`;
+    document.querySelector('#data').appendChild(div);
+})
+
 let send = () => {
     console.log(messageArea.value);
     //socket.emit('message', `message : ${messageArea.value}`);
-    socket.emit('room', roomArea.value, `Message room ${roomArea.value} : ${messageArea.value}`);
+    socket.emit('room', roomArea.value, nameUser,  messageArea.value);
 }
 roomArea.addEventListener('change', (e) => {
-    socket.emit('leave', room);
-    socket.emit('join', e.target.value);
+    socket.emit('leave', room , nameUser);
+    socket.emit('join', e.target.value, nameUser);
     room = e.target.value;
+});
+
+nameArea.addEventListener('change', (e) => {
+    nameUser = e.target.value;
 });
